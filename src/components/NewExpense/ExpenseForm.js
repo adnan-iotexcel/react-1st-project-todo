54d24bee47ede646c91186948dc60c4bef8d30de
adnan-iotexcel <89918638+adnan-iotexcel@users.noreply.github.com>
2021-11-22 17:53:46 +0530

    added two way binding for taken input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -49,6 +49,12 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle('');
+    
+    setEnteredAmount('');
+    
+    setEnteredDate('');
+    
   };
 
   return (
@@ -56,7 +62,10 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input 
+            type='text' 
+            onChange={titleChangeHandler} 
+            value={enteredTitle} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
@@ -64,6 +73,7 @@ const ExpenseForm = () => {
             type='number'
             min='0.01'
             step='0.01'
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -73,6 +83,7 @@ const ExpenseForm = () => {
             type='date'
             min='2019-01-01'
             max='2022-12-31'
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
